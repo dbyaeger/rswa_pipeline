160a86eedc30baa7d083d61cf3ecb4b41e5bb21a
Daniel Yaeger <danielyaeger@Daniels-MacBook-Air-3.local>
2020-05-28 09:24:52 -0700

    Added sleeper level balanced accuracy to predict

--- test_pipeline/predict_from_DNN.py
+++ test_pipeline/predict_from_DNN.py
@@ -39,9 +39,9 @@ def predict(model_name: str, path_to_model: str, path_to_data: str,
                                        batch_size=1, window_size=10)        
     IDs = test_gen.list_IDs      
     test_results_dict = {}
+    test_label_dict = {}
 
     evaluations = []
-
     # iterate over IDs and generate predictions
     for ID in IDs:
         if verbose:
@@ -51,10 +51,10 @@ def predict(model_name: str, path_to_model: str, path_to_data: str,
         y_pred = model.predict(X)
         
         test_results_dict[ID] = y_pred
+        test_label_dict[ID] = y
 
         if evaluate:
           evaluations.append(balanced_accuracy_score(y.argmax(-1),y_pred.argmax(-1)))
-
     
     if not isinstance(save_path, Path): save_path = Path(save_path)
     
@@ -67,8 +67,28 @@ def predict(model_name: str, path_to_model: str, path_to_data: str,
         pickle.dump(test_results_dict, fh)
     
     if evaluate:
-      print(f'Mean balanced accuracy: {np.mean(evaluations)} +/- {np.std(evaluations)}')
+        print(f'Mean balanced accuracy at subsequence level: {np.mean(evaluations)} +/- {np.std(evaluations)} for {len(evaluations)} number of subsequences')
+      
+        sequence_dict_preds = {}
+        sequence_dict_truth = {}
+        sleeper_IDs = list(set([ID.split('_')[0] for ID in list(test_results_dict.keys())]))
+        for ID in sleeper_IDs:
+          subseq = -1
+          while subseq < 1e3:
+            subseq += 1
+            try:
+              if ID not in sequence_dict_preds:
+                sequence_dict_preds[ID] = test_results_dict[f'{ID}_{subseq}']
+                sequence_dict_truth[ID] = test_label_dict[f'{ID}_{subseq}']
+              else:
+                sequence_dict_preds[ID] = np.concatenate((sequence_dict_preds[ID], test_results_dict[f'{ID}_{subseq}']))
+                sequence_dict_truth[ID] = np.concatenate((sequence_dict_truth[ID], test_label_dict[f'{ID}_{subseq}']))
+            except:
+              continue
         
+        evaluations = [balanced_accuracy_score(sequence_dict_truth[ID].argmax(-1),sequence_dict_preds[ID].argmax(-1)) \
+                       for ID in sequence_dict_preds]
+        print(f'Mean balanced accuracy at sleeper level: {np.mean(evaluations)} +/- {np.std(evaluations)} for {len(evaluations)} number of sleepers')
         
         
         
